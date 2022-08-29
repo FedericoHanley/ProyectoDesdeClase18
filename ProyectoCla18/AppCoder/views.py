178d586728da704b24d09dfc9c0eb1ca30de48b3
FedericoHanley <federicohanley.fh@gmail.com>
2022-08-29 20:57:01 -0300

    Clase 19 playground intermidio1

--- ProyectoCla18/AppCoder/views.py
+++ ProyectoCla18/AppCoder/views.py
@@ -9,3 +9,18 @@ def curso(request):
     curso.save()
     texto = f'Curso creado. Nombre es {curso.nombre} y comision es {curso.comision}'
     return HttpResponse(texto)
+
+def inicio(request):
+    return render (request, 'AppCoder/Inicio.html')
+
+def cursos(request):
+    return render (request, 'AppCoder/cursos.html')
+
+def estudiantes(request):
+    return render (request, 'AppCoder/estudiantes.html')
+
+def profesores(request):
+    return render (request, 'AppCoder/profesores.html')
+
+def entregables(request):
+    return render (request, 'AppCoder/entregables.html')
